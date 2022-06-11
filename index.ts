48d2f64a887ce7971bf52747102696a3117833ca
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-06-11 18:56:09 -0400

    Let out a squeel

--- index.ts
+++ index.ts
@@ -1,215 +1,137 @@
-import { randomBytes } from "node:crypto";
 
-export class Node {
-    protected frequency: number = 0
-    private leftNode: Node | undefined
-    private rightNode: Node | undefined
 
-    constructor(leftNode: Node | undefined, rightNode: Node | undefined) {
-        this.leftNode = leftNode
-        this.rightNode = rightNode
-
-        if (typeof leftNode !== "undefined" && typeof rightNode !== "undefined") {
-            this.frequency = leftNode.frequency + rightNode.frequency
-        }
-    }
-
-    public static compare(node1: Node, node2: Node): number {
-        if (node2.frequency === node1.frequency) {
-            return 0
-        }
-        return node1.frequency > node2.frequency ? 1 : -1
-    }
-}
-
-export class Leaf extends Node {
-    private byte: number
-
-    constructor(byte: number, frequency: number) {
-        super(undefined, undefined)
-        this.byte = byte
-        this.frequency = frequency
-    }
-}
-
-export class Huffman {
-    private rootNode: Node | undefined = undefined
-    private input: Buffer
-    private frequencies: {byte: number, frequency: number}[] = []
-    private codes: {byte: number, code: string}[] = []
-    private huffmanTree: Leaf[] = []
-
-    constructor(input: Buffer) {
-        this.input = input
-        this.fillFrequencies()
-    }
-    fillFrequencies() {
-        for (let i = 0; i < this.input.length; i++) {
-            let byte = this.input[i]
-            let code = this.frequencies.find(f => f.byte === byte)
-            if (code) {
-                code.frequency++
-            }
-            else {
-                this.frequencies.push({byte, frequency: 1})
-            }
-        }
-    }
-
-    buildTree() {
-        for (let index = 0; index < this.frequencies.length; index++) {
-            this.huffmanTree.push(new Leaf(this.frequencies[index].byte, this.frequencies[index].frequency))            
-        }
-    }   
-
-    sortTree() {
-        this.huffmanTree.sort(Node.compare)
-    }
-    
-    /**
-     * getTree
-     */
-    public getTree() {
-        return this.huffmanTree        
-    }
-}
+// Much thanks to https://youtu.be/95D99HEn4xU for the clarity of the algorithm process
 
 const bitStream = 'the quick brown fox jumps over the lazy dogthe quick brown fox jumps over the lazy dogthe quick brown fox jumps over the lazy dogthe quick brown fox jumps over the lazy dog'
 
 console.log(`bitStream: ${bitStream}\n`)
 
+let values: number[] = []
 
-const huffmanTree = new Huffman(Buffer.from(bitStream))
-huffmanTree.buildTree()
-huffmanTree.sortTree()
+for (let i = 0; i < bitStream.length; i++) {
+    const bit = bitStream.charCodeAt(i)
+    values.push(bit)
+}
 
-console.dir(huffmanTree.getTree())
+console.log(`values: ${values.join(', ')}\n`)
 
-console.dir(huffmanTree)
+console.log('========================================================\n')
 
-process.exit()
+const compressed = encode(values)
 
+console.log(`compressed: ${compressed.join(', ')}\n`)
 
+const uncompressedValues = decode(compressed)
 
+let uncompressedBitStream: string[] = []
 
-// /**
-//  * 
-//  * @param {string} inStream 
-//  * @param {number} index 
-//  * @param {number} windowSize 
-//  * @returns {number}
-//  */
-// function getWindow(inStream: string, index: number, windowSize: number) {
-//     if (index < windowSize) {
-//         windowSize = index
-//     }
-//     const window = inStream.substring(index - windowSize, index +1)
+for (let i = 0; i < uncompressedValues.length; i++) {
+    const bit = String.fromCharCode(uncompressedValues[i])
+    uncompressedBitStream.push(bit)
+}
 
-//     return window
-    
-// }
+console.log(`uncompressedBitStream: ${uncompressedBitStream.join('')}\n`)
 
-// /**
-//  * What position does the search string first occur at?
-//  * @param {string} searchString 
-//  * @param {string} searchWindow 
-//  * @returns 
-//  */
-// function getMatchIndex(searchString: string, searchWindow: string) {
+function encode(values: number[]): number[] {
+    let foundValue = 0
+    let compressed: number[] = []
 
-//     return searchWindow.indexOf(searchString)
-    
-// }
+    let dictionary: { key: number, value: number }[] = []
 
-// /**
-//  * 
-//  * @param {string} searchString 
-//  * @param {number} startIndex 
-//  * @param {number} searchWindow 
-//  * @param {number} windowSize
-//  * @returns {number}
-//  */
-// function getMatchLength(searchString: string, startIndex: number, searchWindow: string, windowSize: number ) {
-//     console.log('In getMatchLength()')
-//     console.log(searchString, startIndex, searchWindow)
-//     let matchLength = 1
-//     let count = 0
-//     do {
-//         // If we have searched past the windows edge, break
-//         if (count >= windowSize) {
-//             break
-//         }
-//         console.log(`[${searchString.substring(0, startIndex + matchLength)}] === [${searchWindow.substring(startIndex, startIndex + matchLength)}]`, startIndex, matchLength, searchWindow.substring(startIndex, matchLength).startsWith(searchString.substring(startIndex, matchLength)))
-//         count = count + 1
+    let nextDictionaryIndex = 0
 
-//         matchLength = matchLength + 1  
-
-//     } while (searchWindow.substring(startIndex, startIndex + matchLength).startsWith(searchString.substring(0, startIndex + matchLength)) === true);
-//     // Last loop failed, back up by one
-//     const match = searchWindow.substring(startIndex, matchLength - 1)
-//     console.log(`Final match: [${match}]`)
-//     return match.length
-// }
-
-// /******************************************************************************************** 
-//  * Program start 
-//  * *****************************************************************************************/
+    // Fill the dictionary with the first values
+    for (let i = 0; i < 256; i++) {
+        dictionary.push({ key: i, value: i })
+        nextDictionaryIndex = i
+    }
 
 
-// const WINDOW_SIZE = 10
+    for (let i = 0; i < values.length; i++) {
+        const value = values[i]
+        let searchValue = foundValue + value
+        const found = dictionary.find(item => item.value === searchValue)
 
-// console.log('windowSize', WINDOW_SIZE)
+        // Not already in the dictionary
+        if (typeof found === 'undefined') {
+            console.log(`${searchValue} was not found in the dictionary\n`)
+            // Add to the dictionary
+            const newItem = { key: nextDictionaryIndex, value: searchValue }
+            console.log(`newItem: ${JSON.stringify(newItem)}\n`)
+            dictionary.push(newItem)
+            
+            // Reset the found value
+            let previousKey = 0
 
-// const streamLength = bitStream.length
+            // Log the dictionary size
+            console.log(`dictionarySize: ${dictionary.length}\n`)
 
-// console.log('streamLength:', streamLength)
+            // Get the key for the previous value
+            console.log(`Looking for ${foundValue} in the dictionary`)
+            const previousEntry = dictionary.find(item => item.value === foundValue)
 
-// let cursor = 0
-// let searchLength = 1
+            if (typeof previousEntry !== 'undefined') {
+                console.log(`previousEntry: ${JSON.stringify(previousEntry)}\n`)
+                previousKey = previousEntry.key
+            }
 
-// console.log(`starting cursor: ${cursor}\n`)
+            // Add the previous key to the compressed stream
+            console.log(`previousKey: ${previousKey}\n`)
+            compressed.push(previousKey)
+            // Reset the found value to the new value
+            foundValue = value
+            // Increment the next dictionary index
+            nextDictionaryIndex++
+        } else {
+            console.log(`${searchValue} was found in the dictionary\n`)
+            console.log(`found: ${JSON.stringify(found)}\n`)
+            foundValue = searchValue
+        }
+        console.log('========================================================\n')
+    }
 
-// do {
-//     let matchLength = 0
-//     // Emergency break if needed
-//     let count = 1
-//     const inputByte = bitStream.substring(cursor, cursor + searchLength)
+    console.log(`dictionary: ${JSON.stringify(dictionary)}\n`)
 
-//     console.log('cursor:', cursor)
-//     console.log('current byte:', inputByte)
-//     console.log('searchLength:', searchLength)
+    return compressed
+}
 
-//     const window = getWindow(bitStream, cursor, WINDOW_SIZE)
+function decode(compressed: number[]): number[] {
+    let foundValue = 0
+    let uncompressed: number[] = []
 
-//     console.log(`search window: /${window}/`)
+    let dictionary: { key: number, value: number }[] = []
 
-//     let found = false
-    
-//     const positionInWindow = getMatchIndex(inputByte, window)
+    let nextDictionaryIndex = 0
 
-//     if (positionInWindow >= 0) {
-//         // A match was found
-//         found = true
+    // Fill the dictionary with the first values
+    for (let i = 0; i < 256; i++) {
+        dictionary.push({ key: i, value: i })
+        nextDictionaryIndex = i
+    }
 
-//         console.log('!!!!!!!!!! match found in window at position:', positionInWindow)
 
-//         const matchStart = cursor + positionInWindow
-//         console.log(`starting search length check at absolute position: ${matchStart}`)
+    for (let i = 0; i < values.length; i++) {
+        const value = values[i]
+        let searchValue = foundValue + value
+        const previousValue = foundValue
+        const found = dictionary.find(item => item.value === searchValue)
 
-//         // How long does the match extend?
-//         const matchLength = getMatchLength(inputByte, matchStart, bitStream, WINDOW_SIZE)
+        if (typeof found !== 'undefined') {
+            console.log(`${searchValue} was found in the dictionary\n`)
+            uncompressed.push(found.value)
+        } else {
+            console.log(`${searchValue} was not found in the dictionary\n`)
+            // Add to the dictionary
+            const newItem = { key: nextDictionaryIndex, value: searchValue }
+            console.log(`newItem: ${JSON.stringify(newItem)}\n`)
+            dictionary.push(newItem)
 
-//         console.log(`match was n characters long: ${matchLength}\n`)
-//         searchLength = matchStart + matchLength
-//         cursor = cursor + matchLength
-//     } else {
-//         console.log(`==No Match found==: ${cursor}\n`)
-//         cursor = cursor  + 1
-//         matchLength = 0
-//         searchLength = 1
-//     }
-//     count = count + 1
-//     if (count > streamLength) {
-//         break
-//     }
-// } while (cursor < streamLength) 
+            uncompressed.push(value)
+        }
+        console.log('========================================================\n')
+    }
+
+    console.log(`dictionary: ${JSON.stringify(dictionary)}\n`)
+
+    return uncompressed
+}
