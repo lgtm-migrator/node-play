d023272ba607cf158de6b5cb78675e3cf050c30d
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-06-14 20:32:44 -0400

    wip

---
+++ index-lzw.ts
@@ -0,0 +1,50 @@
+
+
+// Much thanks to https://youtu.be/95D99HEn4xU for the clarity of the algorithm process
+// Also thanks to https://www2.cs.duke.edu/csed/curious/compression/lzw.html for the clarity of the algorithm process
+
+import { lzwEncode } from "./lzwEncode"
+import { lzwDecode } from "./lzwDecode"
+
+const bitStream = 'the quick brown fox jumps over the lazy dogthe quick brown fox jumps over the lazy dogthe quick brown fox jumps over the lazy dogthe quick brown fox jumps over the lazy dog'
+
+// const bitStream = 'ba ba ba ba'
+
+console.log(`bitStream: ${bitStream}\n`)
+
+console.log(`bitStream length: ${bitStream.length}\n`)
+
+let values: number[] = []
+
+for (let i = 0; i < bitStream.length; i++) {
+    const bit = bitStream.charCodeAt(i)
+    values.push(bit)
+}
+
+console.log(`values: ${values.join(', ')}\n`)
+
+console.log('========================================================\n')
+
+const compressed = lzwEncode(values)
+
+console.log(`compressed length: ${compressed.length}\n`)
+
+console.log(`compressed: ${compressed.join(', ')}\n`)
+
+console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n')
+
+const uncompressedValues = lzwDecode(compressed)
+
+console.log('========================================================\n')
+
+console.log(`uncompressedValues: ${uncompressedValues.join(', ')}\n`)
+console.dir(uncompressedValues, { depth: null })
+
+let uncompressedBitStream: string[] = []
+
+for (let i = 0; i < uncompressedValues.length; i++) {
+    const bit = uncompressedValues[i].toString(16)
+    uncompressedBitStream.push(bit)
+}
+
+console.log(`uncompressedBitStream: ${uncompressedBitStream.join('')}\n`)
--- index.ts
+++ index.ts
@@ -1,50 +1,142 @@
+// Implimenting https://groups.google.com/g/comp.compression/c/M5P064or93o/m/W1ca1-ad6kgJ?pli=1
 
+const decodeBytes = Buffer.from([0x00, 0x04, 0x82, 0x24, 0x25, 0x8f, 0x80, 0x7f])
 
-// Much thanks to https://youtu.be/95D99HEn4xU for the clarity of the algorithm process
-// Also thanks to https://www2.cs.duke.edu/csed/curious/compression/lzw.html for the clarity of the algorithm process
+let streamIndex = 0
 
-import { lzwEncode } from "./lzwEncode"
-import { lzwDecode } from "./lzwDecode"
+console.log(`decodeBytes: ${decodeBytes.toString('hex')}\n`)
 
-const bitStream = 'the quick brown fox jumps over the lazy dogthe quick brown fox jumps over the lazy dogthe quick brown fox jumps over the lazy dogthe quick brown fox jumps over the lazy dog'
-
-// const bitStream = 'ba ba ba ba'
-
-console.log(`bitStream: ${bitStream}\n`)
-
-console.log(`bitStream length: ${bitStream.length}\n`)
-
-let values: number[] = []
-
-for (let i = 0; i < bitStream.length; i++) {
-    const bit = bitStream.charCodeAt(i)
-    values.push(bit)
+for (let i = 0; i < decodeBytes.length; i++) {
+    const byte = decodeBytes[i]
+    console.log(`byte ${i}: ${byte.toString(16)}`)
 }
-
-console.log(`values: ${values.join(', ')}\n`)
-
 console.log('========================================================\n')
 
-const compressed = lzwEncode(values)
-
-console.log(`compressed length: ${compressed.length}\n`)
-
-console.log(`compressed: ${compressed.join(', ')}\n`)
-
-console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n')
-
-const uncompressedValues = lzwDecode(compressed)
-
-console.log('========================================================\n')
-
-console.log(`uncompressedValues: ${uncompressedValues.join(', ')}\n`)
-console.dir(uncompressedValues, { depth: null })
-
-let uncompressedBitStream: string[] = []
-
-for (let i = 0; i < uncompressedValues.length; i++) {
-    const bit = uncompressedValues[i].toString(16)
-    uncompressedBitStream.push(bit)
+export enum ELiteralMode {
+    'MODE_0' = 'MODE_0',
+    'MODE_1' = 'MODE_1',
 }
 
-console.log(`uncompressedBitStream: ${uncompressedBitStream.join('')}\n`)
+export enum EDictionarySize {
+    '0x04' = 1024,
+    '0x05' = 2048,
+    '0x06' = 4096,
+}
+
+export function getLiteralMode(byte: number): string {
+    if (byte === 0x00) {
+        return ELiteralMode.MODE_0
+    } else if (byte === 0x01) {
+        // return ELiteralMode.MODE_1
+        throw new Error('MODE_1 is not implemented yet')
+    } else {
+        throw new Error(`literalMode: invalid literalMode byte: ${byte.toString(16)}`)
+    }
+}
+
+export function getDictionarySize(byte: number): number {
+    if (byte === 0x04) {
+        return EDictionarySize['0x04']
+    } else if (byte === 0x05) {
+        // return EDictionarySize['0x05']
+        throw new Error('Not implemented')
+    // deepcode ignore DuplicateIfBody: Not implemented yet
+    } else if (byte === 0x06) {
+        // return EDictionarySize['0x06']
+        throw new Error('Not implemented')
+    } else {
+        throw new Error(`dictionarySize: invalid dictionarySize byte: ${byte.toString(16)}`)
+    }
+}
+
+let byteToCheck = decodeBytes[streamIndex]
+streamIndex++
+
+console.log(`byteToCheck: ${byteToCheck.toString(16)}`)
+
+const literalMode = getLiteralMode(byteToCheck)
+
+console.log(`literalMode: ${literalMode}\n`)
+
+byteToCheck = decodeBytes[streamIndex]
+streamIndex++
+
+console.log(`byteToCheck: ${byteToCheck.toString(16)}`)
+
+const dictionarySize = getDictionarySize(byteToCheck)
+
+console.log(`dictionarySize: ${dictionarySize}\n`)
+
+export enum ETokenType {
+    'LITERAL' = 'LITERAL',
+    'DISTANCE_LENGTH' = 'DISTANCE_LENGTH',
+    'END_OF_STREAM' = 'END_OF_STREAM',
+}
+
+export function getTokenType(byte: number): ETokenType {
+    if (byte === 0xFF) {
+        // return ETokenType.END_OF_STREAM
+        throw new Error('END_OF_STREAM is not implemented yet')
+    } else if ( byte & 0x01) {
+        // return ETokenType.DISTANCE_LENGTH
+        throw new Error('DISTANCE_LENGTH is not implemented yet')
+    } else {
+        return ETokenType.LITERAL
+    } 
+    
+}
+
+export interface IToken {
+    tokenType: ETokenType,
+    literalMode: ELiteralMode,
+    literal: number,
+    value: number,
+    distance: number,
+    length: number
+}
+
+export function generateLiteralCodesMode0(): IToken[] {
+    const literalCodes: IToken[] = []
+    for (let i = 0; i < 256; i++) {
+        const newToken: IToken = {
+            tokenType: ETokenType.LITERAL,
+            literalMode: ELiteralMode.MODE_0,
+            literal: i,
+            value: i,
+            distance: 0,
+            length: 0
+        }
+        literalCodes.push(newToken)
+    }
+    return literalCodes
+}
+
+const literalCodesMode0 = generateLiteralCodesMode0()
+
+const LiteralTokens: Record<string, IToken[]> = {
+    MODE_0: literalCodesMode0,
+    MODE_1: []
+}
+
+export function getLiteralToken(mode: string, literal: number): IToken {
+    const literalCodes = LiteralTokens[mode]
+
+    console.log(`literal prior to type removal: ${literal.toString(16)}`)
+
+    // Clear the literal type bit
+    literal = literal >>> 1
+
+    return literalCodes[literal]
+}
+
+while (streamIndex < decodeBytes.length) {
+    const byte = decodeBytes[streamIndex]
+    streamIndex++
+    const tokenType = getTokenType(byte)
+    if (tokenType === ETokenType.LITERAL || tokenType === ETokenType.DISTANCE_LENGTH) {
+        const literal = getLiteralToken(literalMode, byte)
+        console.log(`literal: ${literal.value}`)
+    } else if (tokenType === ETokenType.END_OF_STREAM) {
+        throw new Error('END_OF_STREAM is not implemented yet')
+    }
+}
